82a67bc7fdf520bac263048dddaf34fbffc16b5c
jmccormack1717 <118073033+jmccormack1717@users.noreply.github.com>
2025-09-13 21:57:37 +0000

    s

--- backend/github_helpers.py
+++ backend/github_helpers.py
@@ -1,14 +1,20 @@
+import os
 from github import Github
 from urllib.parse import urlparse
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 def get_readme_from_repo(repo_url: str):
     try:
         path = urlparse(repo_url).path.strip('/')
-        g = Github()  # Can pass token here for higher rate limit
+        if GITHUB_TOKEN:
+            g = Github(GITHUB_TOKEN)  # Authenticated client with token
+        else:
+            g = Github()  # Unauthenticated client
+
         repo = g.get_repo(path)
         readme = repo.get_readme()
         return readme.decoded_content.decode()
     except Exception as e:
         print(f"GitHub fetch failed: {e}")
         return None
-
